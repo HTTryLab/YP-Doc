925b80b8a82d0505ba76307c951eed5783b5eb8d
HTTryLab <HTTryLab@users.noreply.github.com>
2026-04-29 23:55:14 +0800

    Fix: Move products-data.js to data folder

--- src/pages/products.js
+++ src/pages/products.js
@@ -3,7 +3,7 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import products from './products-data';
+import products from '../data/products-data';
 
 import styles from './index.module.css';
 
